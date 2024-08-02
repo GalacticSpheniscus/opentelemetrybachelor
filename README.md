2c455dab3a0f37da0429356b51596e3e4ae27d64
leo <leo@leo.com>
2024-08-02 19:42:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# OpenTelemetry Inspection
+
+Dieses Repository enthält den gesamten Code, sowie alle Konfigurationsdateien, die für die Bachelorarbeit "Einsatz des OpenTelemetry Observability Standards in der Generali Deutschland AG" relevant sind.
+
+## OpenTelemetry Kubernetes Operator
+
+Die in der Bachelorarbeit stattgefundenen Untersuchungen zu dem OpenTelemetry Operator sind in dem Verzeichnis "oteloperator" zu finden. Die in in diesem Zusammenhang
+untersuchte Anwendung Google Microservice Demo, sowie die dazugehörigen Helm-Charts sind in dem Verzeichnis "microservices-demo-main" zu finden.
+
+Bemerkung zu den Helm Charts der Google Microservice Demo App: Es sind zwei Versionen des Helm-Charts zu finden. In der originellen ist die OpenTelemetry
+Instrumentierung mit dem Operator nicht inkludiert. In dem modifizierten Helm-Chart wurden die notwendigen OpenTelemetry Annotations bei den einzelnen Microservices
+eingefügt, sodass die Automatische Injezierung durchgeführt wird.
+
+## Aufbau K8s Cluster
+
+Die Vagrant- und Shell-Skripte für den Aufbau des K8s Clusters sind in dem Verzeichnis "vagrant_setup" zu finden.
+
+## OpenTelemetry Demo App
+
+Der Source Code ist unverändert zu der originellen OpenTelemetry Demo App in dem Verzeichnis "opentelemetry-demo-main" zu finden.
+Zusätzlich wurden auch die Helm-Charts hinzugefügt, über die die Anwendung im Cluster deployed werden kann.
+
+
---
+++ vagrant_setup/generic_vm_setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ ! -d /etc/systemd/resolved.conf.d ]; then
+	sudo mkdir /etc/systemd/resolved.conf.d/
+fi
+cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
+[Resolve]
+DNS=8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1
+EOF
+
+sudo systemctl restart systemd-resolved
+
+sudo swapoff -a
+
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
+
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get update -y
+apt-get install -y software-properties-common curl apt-transport-https ca-certificates
+
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" |
+    tee /etc/apt/sources.list.d/cri-o.list
+
+sudo apt-get update -y
+sudo apt-get install -y cri-o
+
+sudo systemctl daemon-reload
+sudo systemctl enable crio --now
+sudo systemctl start crio.service
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update -y
+sudo apt-get install -y kubelet="1.30.3-*" kubectl="1.30.3-*" kubeadm="1.30.3-*"
+sudo apt-get update -y
+sudo apt-get install -y jq
+
+sudo apt-mark hold kubelet kubectl kubeadm cri-o
+
+local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
+cat > /etc/default/kubelet << EOF
+KUBELET_EXTRA_ARGS=--node-ip=$local_ip
+EOF
